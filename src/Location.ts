1875bfb0d94fe4126bf98834d40cfbea7f48e9d1
Changyuan Lin <coreballium@outlook.com>
2021-08-05 21:36:04 -0600

    Sample locations appear

---
+++ src/Location.ts
@@ -0,0 +1,7 @@
+type Coordinate = [number, number]
+
+export interface Location {
+  id: string
+  name: string
+  coordinates: Coordinate[]
+}
--- src/MapView.tsx
+++ src/MapView.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import ReactMapboxGl from 'react-mapbox-gl'
+import ReactMapboxGl, { Layer, Source } from 'react-mapbox-gl'
 import { createStyles, makeStyles, Theme } from '@material-ui/core'
+import { Location } from './Location'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,6 +21,18 @@ const Map = ReactMapboxGl({
 
 export const MapView = () => {
   const classes = useStyles()
+  const sampleLocations: Location[] = require('./sample-locations.json')
+
+  const toGeoJson = (location: Location) => ({
+    type: 'geojson',
+    data: {
+      type: 'Feature',
+      geometry: {
+        type: 'Polygon',
+        coordinates: location.coordinates,
+      },
+    },
+  })
 
   return (
     <div className={classes.root}>
@@ -28,7 +41,23 @@ export const MapView = () => {
         style="mapbox://styles/mapbox/streets-v11" // eslint-disable-line react/style-prop-object
         center={[-76.48, 42.45]}
         zoom={[14.5]}
-      />
+      >
+        {sampleLocations.map(location => (
+          <>
+            <Source id={location.id} geoJsonSource={toGeoJson(location)} />
+            <Layer
+              id={`${location.id}-fill`}
+              type="fill"
+              sourceId={location.id}
+              onClick={() => console.log(location.id)}
+              paint={{
+                'fill-color': '#0080ff',
+                'fill-opacity': 0.5,
+              }}
+            />
+          </>
+        ))}
+      </Map>
     </div>
   )
 }
