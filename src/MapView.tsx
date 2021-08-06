a9a92979757ae3c757d0fc56f1055027f6b85041
Changyuan Lin <coreballium@outlook.com>
2021-08-05 20:37:02 -0600

    Convert to react-mapbox-gl

--- src/MapView.tsx
+++ src/MapView.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useRef } from 'react'
-import mapboxgl from 'mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
+import React from 'react'
+import ReactMapboxGl from 'react-mapbox-gl'
 import { createStyles, makeStyles, Theme } from '@material-ui/core'
-
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN!
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -15,27 +13,22 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 )
 
+const Map = ReactMapboxGl({
+  accessToken: process.env.REACT_APP_MAPBOX_TOKEN!,
+  hash: true,
+})
+
 export const MapView = () => {
   const classes = useStyles()
-  const mapContainer = useRef<HTMLDivElement>(null)
-  const map = useRef<mapboxgl.Map | null>(null)
-  const lng = -76.48
-  const lat = 42.45
-  const zoom = 14.5
-
-  useEffect(() => {
-    if (map.current || !mapContainer.current) return
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
-    })
-  })
 
   return (
     <div className={classes.root}>
-      <div ref={mapContainer} className={classes.map}></div>
+      <Map
+        className={classes.map}
+        style="mapbox://styles/mapbox/streets-v11" // eslint-disable-line react/style-prop-object
+        center={[-76.48, 42.45]}
+        zoom={[14.5]}
+      />
     </div>
   )
 }
